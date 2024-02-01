1ab3fe79cd190b07b83a8a345bb61de0b59aab0c
Nikita Gharin <77896451+cvclon3@users.noreply.github.com>
2024-02-01 22:39:52 +0300

    add basic middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	// SERVEMUX
 	mux := http.NewServeMux()
 
@@ -17,5 +21,8 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	// MIDDLEWARES
+	midwares := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return midwares.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.21.2
 require github.com/go-sql-driver/mysql v1.7.1
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/justinas/alice v1.2.0
